181bab092d7f0cc44f03303211356294da34ced0
ubuntu <quentinrougemont@orange.fr>
2018-03-09 19:02:08 -0500

    add vcf_to_treemix.sh

---
+++ 01.scripts/utility_scripts/vcf_to_treemix.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+input=$1
+cluster=$2
+
+if [  -z "$input" ]
+then
+	echo "error please provide vcffile"
+	exit
+fi 
+
+if [  -z "$cluster" ]
+then
+	echo "error please provide strat file"
+	exit
+fi 
+
+#plink --vcf "$input" --allow-extra-chr --recode
+
+awk '{print $1, $1"_"$2 }' plink.ped > pop_ind.tmp
+cut -d " " -f 3- plink.ped > geno.tmp
+cp plink.ped > plink.ped.back
+paste pop_ind.tmp geno.tmp  > plink.ped
+rm *.tmp
+
+plink --file plink \
+	--allow-extra-chr \
+	--freq --missing --within "$cluster" \
+	--out plink
+
+gzip plink.frq.strat
+./01.scripts/utility_scripts/plink2treemix.py plink.frq.strat.gz treemix.frq.gz
