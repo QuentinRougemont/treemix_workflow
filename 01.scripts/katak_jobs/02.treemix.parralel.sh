5df87b6924a9fcc90859d57b71094f2248d04a67
ubuntu <quentinrougemont@orange.fr>
2019-06-06 06:11:46 -0400

    update katak script

--- 01.scripts/katak_jobs/02.treemix.parralel.sh
+++ 01.scripts/katak_jobs/02.treemix.parralel.sh
@@ -2,9 +2,7 @@
 #SBATCH -J "se_treemix"
 #SBATCH -o log_%j
 #SBATCH -c 1
-#SBATCH -A ibismax
-#SBATCH -p ibismax
-##SBATCH -p low-cancel
+#SBATCH -p large
 ##SBATCH --mail-type=FAIL
 ##SBATCH --mail-user=asdf
 #SBATCH --time=10-00:00
@@ -12,8 +10,6 @@
 
 # Load the software with module if applicable:
 module load treemix
-
-
 
 mig=$1 #number of migration event to test 
 if [ -z "$mig" ]
@@ -33,4 +29,4 @@ then
     sleep 1s
 fi
 
-./01-scripts/02.treemix.parralel.sh $mig $rout
+./01.scripts/02.treemix.parralel.sh $mig $rout
